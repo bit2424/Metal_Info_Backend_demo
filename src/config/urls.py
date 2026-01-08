c6916a7eb8016e9759e41e2c4d71460bdb4d94cc
bit2424 <nelsonquinones2424@gmail.com>
2026-01-08 16:41:28 +0100

    fix: Correct admin URL path in Django configuration

--- src/config/urls.py
+++ src/config/urls.py
@@ -5,6 +5,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.admin_view),
+    path("admin/", admin.site.urls),
     path("api/v1/", include("metal_prices.api.v1.urls")),
 ]
